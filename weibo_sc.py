3613f5221ce366bbb59f51e1036efdefa3eeb18b
iawes <iawes.g@gmail.com>
2023-03-08 13:33:57 +0800

    add senm mail

--- weibo_sc.py
+++ weibo_sc.py
@@ -10,10 +10,18 @@ import schedule
 
 import argparse
 
+
+#import socks
+#'proxy_port' should be an integer
+#'PROXY_TYPE_SOCKS4' can be replaced to HTTP or PROXY_TYPE_SOCKS5
+#socks.setdefaultproxy(socks.PROXY_TYPE_HTTP, "10.158.100.9", 8080)
+#socks.wrapmodule(requests)
+
 from weibo_dongtu import new_charts
 from weibo_video import convert_video_js
 from html_to_video_2 import html_to_video
 from webm_to_mp4 import webm_to_mp4
+from qq_email import qq_send_mail
 
 #def run():
 #    print("I'm doing something...")
@@ -34,12 +42,17 @@ os.environ["https_proxy"] = "http://10.158.100.9:8080"
 #os.environ["http_proxy"] = "http://135.252.244.221:3128"
 #os.environ["https_proxy"] = "http://135.252.244.221:3128"
 
+#proxies = { "http": "http://10.158.100.9:8080", "https": "http://10.158.100.9:8080", }
+#proxies = { "http": "http://135.251.33.16:80", "https": "http://135.251.33.16:80", }
+
 def get_trs():
     url = 'https://s.weibo.com/top/summary?cate=realtimehot'
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0',
-        'Cookie': 'SINAGLOBAL=1841149334224.8252.1642397316953; SUB=_2AkMVODNxf8NxqwFRmP4dxWjkb4xxzw3EieKjZMKqJRMxHRl-yT9jqhJctRB6PrgdnsJWNs-mEM0FvrXkkj_GbQ92SyMy; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9WhUHi5ZivCRoPRwfx_.UyF6; login_sid_t=21ee6d671cc1f4245087df89ce7770b5; cross_origin_proto=SSL; _s_tentry=passport.weibo.com; Apache=1841149334224.8252.1642397316953; ULV=1676516741382:1:1:1:1841149334224.8252.1642397316953:'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+        'Connection':'close',
+        #'Cookie': 'SINAGLOBAL=1841149334224.8252.1642397316953; SUB=_2AkMVODNxf8NxqwFRmP4dxWjkb4xxzw3EieKjZMKqJRMxHRl-yT9jqhJctRB6PrgdnsJWNs-mEM0FvrXkkj_GbQ92SyMy; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9WhUHi5ZivCRoPRwfx_.UyF6; ULV=1676516741382:1:1:1:1841149334224.8252.1642397316953:; UOR=,,www.baidu.com; login_sid_t=21ee6d671cc1f4245087df89ce7770b5; cross_origin_proto=SSL; _s_tentry=passport.weibo.com; Apache=1841149334224.8252.1642397316953'
     }
+    #response = requests.get(url=url, headers=headers, proxies=proxies)
     response = requests.get(url=url, headers=headers)
     #print(response)
     selector = parsel.Selector(response.text)
@@ -58,7 +71,8 @@ class WeiboHot(object):  # 创建Circle类
 
     def __del__(self):
         #schedule.clear(self.schedule_tag)
-        self.fd.close()
+        if self.fd != -1:
+            self.fd.close()
         print('clear done.')
 
     def new_day(self, interval):
@@ -94,29 +108,29 @@ class WeiboHot(object):  # 创建Circle类
         now_time = int(time.time())
         timeArray = time.localtime(now_time)
         date = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-        try:
-            trs = get_trs()
-            print(len(trs))
-            for tr in trs:
-                num = tr.css('td.td-01.ranktop::text').get()
-                #print(num)
-                if num:
-                    if num.isdigit():
-                        title = tr.css('.td-02 a::text').get()
-                        hot2 = tr.css('.td-02 span::text').get()
-                        hot = re.sub('([^\u0030-\u0039])', '', hot2)
-                        dit = {
-                            '时间': date,
-                            '排名': num,
-                            '标题': title,
-                            '热度': hot,
-                        }
-                        #print(dit)
-                        self.csv_writer.writerow(dit)
-                    else:
-                        print("not digit num.")
-        except:
-            print('get_trs failed.')
+#    try:
+        trs = get_trs()
+        #print(len(trs))
+        for tr in trs:
+            num = tr.css('td.td-01.ranktop::text').get()
+            #print(num)
+            if num:
+                if num.isdigit():
+                    title = tr.css('.td-02 a::text').get()
+                    hot2 = tr.css('.td-02 span::text').get()
+                    hot = re.sub('([^\u0030-\u0039])', '', hot2)
+                    dit = {
+                        '时间': date,
+                        '排名': num,
+                        '标题': title,
+                        '热度': hot,
+                    }
+                    print(dit)
+                    self.csv_writer.writerow(dit)
+                else:
+                    print("not digit num.")
+#    except:
+#        print('get_trs failed.')
 
     def process_day(self, interval):
 
@@ -124,7 +138,7 @@ class WeiboHot(object):  # 创建Circle类
             schedule.cancel_job(self.job)
             self.job = None
 
-        self.job = schedule.every(interval).minutes.do(self.get_content)
+        self.job = schedule.every(interval).minutes.until('22:00').do(self.get_content)
 
         #while True:
         #    schedule.run_pending()
@@ -135,57 +149,76 @@ def train_options():
     parser = argparse.ArgumentParser()
     parser.add_argument("--interval", default=2, type=int, help='interval of timer')
     parser.add_argument("--schedule_on", default=False, type=bool, help='start timer')
+    parser.add_argument("--spy_only", default=True, type=bool, help='start timer')
+    parser.add_argument("--retuest_test", default=False, type=bool, help='retuest_test')
     #parser.add_argument("--max_features", default=6, type=int, help='maximum of features',)
     #parser.add_argument("--max_depth", default=5, type=int,help='maximum depth')
     opt = parser.parse_args()
     return opt
 
-def process_day(interval):
-    #new day content
+def process_day(interval, spy_only):
+    all_jobs = schedule.get_jobs()
+    print(all_jobs)
+
+    print('new day content')
     hot.new_day(interval)
 
-    yes = (datetime.datetime.now() + datetime.timedelta(days = -1)).strftime('%Y-%m-%d')
-    print(yes)
-    yes_csv = './weibo/' + yes + '.csv'
-    try:
-        new_charts(yes_csv)
-    except:
-        print('new_charts failed.')
+    all_jobs = schedule.get_jobs()
+    print(all_jobs)
 
-    try:
-        convert_video_js(yes_csv + '.html', r'C:\usr\code\pytdx\convert_video_template.html', 15, 4, 30, 31)
-    except:
-        print('convert_video_js failed.')
+    if spy_only == False:
 
-    filename = 'file:///'+os.getcwd()+'/weibo/' + yes + '.video.html'
-    print(filename)
-    try:
-        html_to_video(filename, 600)
-    except:
-        print('html_to_video failed.')
+        yes = (datetime.datetime.now() + datetime.timedelta(days = -1)).strftime('%Y-%m-%d')
+        print(yes)
+        yes_csv = './weibo/' + yes + '.csv'
+        try:
+            new_charts(yes_csv)
+        except:
+            print('new_charts failed.')
 
-    time.sleep(300)
-    webm_path = r'C:\N-20S1PF344DFM-Data\yaweili\Downloads\\' + yes + '.webm'
-    mp4_path = r'C:\N-20S1PF344DFM-Data\yaweili\Downloads\\' + yes + '.mp4'
-    try:
-        webm_to_mp4(webm_path, mp4_path)
-    except:
-        print('webm_to_mp4 failed.')
+        try:
+            convert_video_js(yes_csv + '.html', r'C:\usr\code\pytdx\convert_video_template.html', 15, 4, 30, 31)
+        except:
+            print('convert_video_js failed.')
+
+        filename = 'file:///'+os.getcwd()+'/weibo/' + yes + '.video.html'
+        print(filename)
+        try:
+            html_to_video(filename, 600)
+        except:
+            print('html_to_video failed.')
+
+        time.sleep(300)
+        webm_path = r'C:\N-20S1PF344DFM-Data\yaweili\Downloads\\' + yes + '.webm'
+        mp4_path = r'C:\N-20S1PF344DFM-Data\yaweili\Downloads\\' + yes + '.mp4'
+        try:
+            webm_to_mp4(webm_path, mp4_path)
+        except:
+            print('webm_to_mp4 failed.')
+
+        time.sleep(300)
+        try:
+            qq_send_mail(yes, mp4_path)
+        except:
+            print('qq_send_mail failed.')
 
     print(hot.day)
     print(hot.csv_file)
     print(hot.csv_writer)
     print(hot.fd)
     print(hot.job)
-    print(yes_csv)
 
 if __name__ == "__main__":
 
     opt = train_options()
     print(opt)
 
+    if opt.retuest_test == True:
+        get_trs()
+        exit()
+
     # 开始执行任务先
-    process_day(opt.interval)
+    process_day(opt.interval, opt.spy_only)
 
     if opt.schedule_on == True:
         # 每天生成新的 csv 和 html
